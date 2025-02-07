e1df3be454a8d82c33847632337939f8898cb112
Mr. Leo <leominton88@gmail.com>
2025-02-07 13:26:34 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,12 +5,6 @@ terraform {
       version = "~> 5.0"
     }
   }
-  backend "s3" {
-    bucket = "terraform-labs-backend-et9kziex"
-    key = "iam-user/terraform.tfstate"
-    region = "us-east-1"
-    encrypt = true
-  }
 }
 
 resource "aws_iam_user" "numbered_users" {
